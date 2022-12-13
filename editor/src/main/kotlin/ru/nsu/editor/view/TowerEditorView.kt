f7980fe2ea8023cc6a6f71753593bb8e1a21f281
anast3t <bortnikov20000@mail.ri>
2022-12-14 04:11:21 +0700

    settings components

--- editor/src/main/kotlin/ru/nsu/editor/view/TowerEditorView.kt
+++ editor/src/main/kotlin/ru/nsu/editor/view/TowerEditorView.kt
@@ -2,33 +2,44 @@ package ru.nsu.editor.view
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import javafx.beans.property.Property
+import javafx.beans.property.SimpleObjectProperty
 import javafx.collections.FXCollections
 import javafx.geometry.Insets
+import javafx.scene.control.ComboBox
 import javafx.scene.control.SkinBase
 import javafx.scene.layout.VBox
 import org.w3c.dom.Node
+import ru.nsu.editor.view.component.SettingsComponent
+import ru.nsu.editor.view.component.SettingsComponent2
+import ru.nsu.editor.view.component.TowerSettingsComponent
 import ru.nsu.editor.view.component.TowerUpgradeComponent
 import ru.nsu.lib.common.TowerData
 import ru.nsu.lib.common.TowerUpdate
 
 import tornadofx.*
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class TowerEditorView : View("Tower editor") {
-    private var towerData = TowerData("",
-        mutableListOf(
-            TowerUpdate(0, 0, 0, 0, 0, 0)
-        )
-    )
-    private var towerUpgradeComponents = mutableListOf<TowerUpgradeComponent>()
-    private lateinit var upgradeStack:VBox
+    private var menuValue = SimpleObjectProperty<LayoutType>()
+    private val mapper = jacksonObjectMapper()
+    private val jsonFolder = "./jsons"
 
+    private lateinit var settings: SettingsComponent2<TowerData>
+    private lateinit var settingsParent: VBox
+    private fun refreshSettings(new: SettingsComponent2<TowerData>){
+        settings.removeFromParent()
+        settings = new
+        settingsParent.add(settings)
+    }
     init {
-        val mapper = jacksonObjectMapper()
-//        mapper.writeValue(
-//            File("./configuration/test.json"),
-//            TowerData
-//        )
+        menuValue.value = LayoutType.NONE
+        menuValue.onChange {
+            println(menuValue.value)
+        }
+        settings = TowerSettingsComponent()
     }
 
     override val root = borderpane {
@@ -39,67 +50,54 @@ class TowerEditorView : View("Tower editor") {
                     replaceWith<MainMenuView>()
                 }
                 button("Save").action {
+                    Files.createDirectories(Paths.get(jsonFolder, "/$menuValue/"))
+                    println(Paths.get(jsonFolder, "/$menuValue/test.json").toFile())
+                    mapper.writeValue(Paths.get(jsonFolder, "/$menuValue/test.json").toFile(), settings.getSettings())
+                }
+                button("Import").action {
 
                 }
-                button("Import")
+                combobox(menuValue, FXCollections.observableArrayList(*LayoutType.values()))
             }
         }
         left {
-            button("Left")
+            vbox{
+                squeezebox {
+                    fold("Tower files", expanded = true){
+                        listview<String> {
+                            items.add("Alpha")
+                            items.add("Beta")
+                            items.add("Gamma")
+                            items.add("Delta")
+                            items.add("Epsilon")
+                        }
+                    }
+                    fold("Sprites", expanded = true){
+                        listview<String> {
+                            items.add("Alpha")
+                            items.add("Beta")
+                            items.add("Gamma")
+                            items.add("Delta")
+                            items.add("Epsilon")
+                        }
+                    }
+                }
+            }
         }
         right {
-            vbox {
-                squeezebox {
-                    fold("Visuals", expanded = true){
-                        form {
-                            fieldset {
-                                field("Tower sprite"){
-                                    textfield(towerData.file)
-                                }
-                                field("Projectile sprite TBD"){
-                                    textfield()
-                                }
-                            }
-                        }
-                    }
-                }
-                scrollpane {
-                    upgradeStack = vbox{
-                        val new = TowerUpgradeComponent(0)
-                        towerUpgradeComponents.add(new)
-                        add(new)
-                    }
-                }
-                borderpane{
-                    left = hbox{
-                        button("Add upgrade"){
-                            action {
-//                                towerData.upgrades.add(TowerUpdate(0,0,0,0,0,0)) TODO: pull data from components only on save
-                                val newComponent = TowerUpgradeComponent(towerUpgradeComponents.size)
-                                towerUpgradeComponents.add(newComponent)
-                                upgradeStack.add(newComponent)
-                            }
-                            hboxConstraints {
-                                marginTop = 10.0
-                            }
-                        }
-                    }
-                    right = hbox{
-                        button("Remove"){
-                            action {
-                                towerUpgradeComponents.removeLast().removeFromParent()
-                            }
-                            hboxConstraints {
-                                margin = Insets(10.0)
-                            }
-                        }
-                    }
-                }
+            settingsParent = vbox{
+                add(settings)
+            }
+        }
+        center {
+            rectangle {
+                width=200.0
+                height=200.0
             }
         }
     }
 
-    enum class TowerType{
-        AOE, TARGET
+    private enum class LayoutType{
+        NONE, TOWER, ENEMY, MAP, WAVE
     }
 }
