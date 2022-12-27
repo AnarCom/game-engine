04f9a62e12c6f9e2dd73c5ffd22a93ca972f7429
anarcom <a.dolgii@g.nsu.ru>
2022-12-27 19:52:25 +0700

    added money increasing functionality

--- engine/src/main/kotlin/ru/nsu/engine/engine/entity/Enemy.kt
+++ engine/src/main/kotlin/ru/nsu/engine/engine/entity/Enemy.kt
@@ -16,26 +16,23 @@ class Enemy(
     )
 
     init {
-//        imageView.x = enemyPath[0].x.toDouble()
-//        imageView.y = enemyPath[0].y.toDouble()
         imageView.fitHeight = 40.0
         imageView.fitWidth = 40.0
-        this.healthAtDeletion = -3
+        healthAtDeletion = enemyType.enemyAttack
     }
 
     fun decreaseHp(damage: Int) {
         synchronized(this) {
             hp -= damage
-            if(hp <= 0){
+            if (hp <= 0) {
                 healthAtDeletion = 0
+                moneyAtDeletion = enemyType.gold
                 canBeDeleted = true
-                println("killed")
             }
         }
     }
 
     override fun tickHandler(engine: Engine) {
-
     }
 
     fun getPosition(): Pair<Double, Double> = Pair(imageView.translateX, imageView.translateY)
