06504b24dfbb94ce2801e713b33f1b2fc889893b
anarcom <a.dolgii@g.nsu.ru>
2022-12-27 19:12:18 +0700

    added support of modal windows

--- engine/src/main/kotlin/ru/nsu/engine/view/LevelSelectView.kt
+++ engine/src/main/kotlin/ru/nsu/engine/view/LevelSelectView.kt
@@ -7,7 +7,6 @@ import ru.nsu.lib.LevelData
 import tornadofx.*
 import java.io.File
 
-//TODO: отображение isDisabled уровня по тому, пройден ли он или нет
 class LevelSelectView : View("My View") {
     private val levels: List<LevelData>
 
@@ -24,10 +23,12 @@ class LevelSelectView : View("My View") {
             gridpane {
                 for (i in levels) {
                     row {
-                        button(i.name){
+                        button(i.name) {
                             action {
                                 GameState.levelData = i
-                                replaceWith<GameView>()
+//                                replaceWith<GameView>()
+                                val modal = find<GameView>()
+                                modal.openModal()
                             }
                         }
                     }
